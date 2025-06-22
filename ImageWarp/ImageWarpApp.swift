6a39e0b49b880fd25cd19554b6d979eeb7ec671d
saechaol <33850575+saechaol@users.noreply.github.com>
2025-06-21 23:13:54 -0700

    Initial Commit

---
+++ ImageWarp/ImageWarpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageWarpApp.swift
+//  ImageWarp
+//
+//  Created by 세차오 루카스 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageWarpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
